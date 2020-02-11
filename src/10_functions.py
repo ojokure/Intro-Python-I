c62d83203767e29196bc8da96b05d7d3458a78ed
oladimeji-ojo <ojokuredim@gmail.com>
2020-02-11 11:07:12 +0000

    functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -4,9 +4,16 @@
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
-num = int(num)
 
+
+def checkType(n):
+    if int(n) % 2 == 0:
+        print('Even')
+    else:
+        print('Odd')
+
+
+checkType(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
